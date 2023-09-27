2a2317f6f5bed1584c08a888b2017f52c10afd10
Naveen Singh <naveensingh7604@gmail.com>
2023-09-28 00:18:37 +0530

    Update Home.jsx

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -13,7 +13,7 @@ const Home = () => {
   return (
     <>
       <div className='h-[91vh]' style={style}>
-        <div className='text-left h-[91vh]  md:text-6xl lg:text-7xl sm:text-4xl text-white ' style={{background: "linear-gradient(90deg, rgba(0,0,0,0.8548669467787114) 0%, rgba(0,0,0,0.4599089635854342) 32%, rgba(255,252,252,0) 100%"}}>
+        <div className='text-left h-[91vh]  md:text-6xl text-2xl lg:text-7xl sm:text-4xl text-white ' style={{background: "linear-gradient(90deg, rgba(0,0,0,0.8548669467787114) 0%, rgba(0,0,0,0.4599089635854342) 32%, rgba(255,252,252,0) 100%"}}>
          <p className='py-36 px-6'> We Provide Solar Solutions <br />  At Your Doorstep</p>
         </div>
       </div>
