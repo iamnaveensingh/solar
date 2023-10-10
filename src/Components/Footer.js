10a0430c835c9f5e8294c703a72844668e0b43c7
Naveen Singh <naveensingh7604@gmail.com>
2023-10-10 13:32:58 +0530

    update

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,11 +1,12 @@
 import React from "react";
 
 export default function App() {
+  const year = new Date().getFullYear();
   return (
     <footer  class="flex bg-gray-200 dark:text-white dark:bg-gray-800  flex-col items-center  text-center text-black">
 
       <div class="w-full p-4 text-center border-t border-gray-200  dark:border-gray-700 ">
-       &copy; 2023 All Copyrights Recived.  	&nbsp;
+       &copy; {year} All Rights Recived.  	&nbsp;
         <a
           class="text-neutral-800 dark:text-neutral-400 sm:inline block"
           href="/"
